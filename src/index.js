0d8ef8c7f5ea052b2bc787dc8118073fc3e7a115
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2021-01-20 09:55:05 +0200

    сделал правильное хранение в локалсторейдж

--- src/index.js
+++ src/index.js
@@ -2,27 +2,20 @@ import css from "./style.css";
 import menu from './menu.json'
 import tamplates from './tamplates/menu.hbs'
 const checkbox = document.querySelector('.theme-switch__toggle')
-
 const body = document.querySelector('body')
-
 const Theme = {
   LIGHT: "light-theme",
   DARK: "dark-theme",
 };
 
 const listItem = document.querySelector('.js-menu')
-console.log(listItem);
-
 const markup = tamplates(menu)
-console.log(markup);
 
 listItem.insertAdjacentHTML('beforeend', markup)
 
-
-
 checkbox.addEventListener('change', () => {
   if (checkbox.checked) {
-    localStorage.setItem('Theme', " ")
+    localStorage.setItem('Theme', 'Dark')
     body.classList.add(Theme.DARK)
     body.classList.remove(Theme.LIGHT);
   } else {
@@ -32,7 +25,7 @@ checkbox.addEventListener('change', () => {
   }
 })
 
- if (localStorage.getItem("Theme")) {
+if (localStorage.getItem("Theme")) {
   body.classList.add(Theme.DARK);
   checkbox.checked = true;
 }
